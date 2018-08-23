cb5d4c2ff0011e5c4242df3d96ce7eb8e6dd4cc5
Install Jekyll <iam-mac2@iam-mac2s-mbp.homenet.telecomitalia.it>
2018-08-23 09:04:56 +0200

    Add exercises.

---
+++ exercise-2.2/scripts/js/main-min.js
@@ -0,0 +1,1 @@
+var app=new Vue({el:"#app",data:{counter:0,result:""},methods:{updateResult:function(){return this.counter>5?"Greater than 5":"Smaller than 5"}}});
---
+++ exercise-2.3/scripts/js/main.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    stCounter: 0,
+    ndCounter: 0,
+    result: '',
+  },
+  computed: {
+    // rely on computed to cash data properties and execute functions only when needed
+    resultComputed: function() {
+      console.log('Computed.')
+      return this.stCounter > 5 ? 'Greater than 5' : 'Smaller than 5';
+    }
+  },
+  watch: {
+    stCounter: function(value) {
+      var vm = this;
+      setTimeout(function() {
+        vm.stCounter = 0;
+      }, 2000);
+    }
+  },
+  methods: {
+    resultMethod: function() {
+      console.log('Method.')
+      return this.stCounter > 5 ? 'Greater than 5' : 'Smaller than 5';
+    }
+  }
+});
